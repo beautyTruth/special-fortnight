74577a3fd4223bf6b0ef7163de5f073b30acd1cf
Phil <happy.melodist@gmail.com>
2021-12-15 23:39:16 -0600

    review dynamic global variable strict mode

--- 3.dynamic-global-varaible+strict-mode.js
+++ 3.dynamic-global-varaible+strict-mode.js
@@ -1,14 +1,16 @@
-'use strict'
+// "use strict";
 
-function otherBook() {
-  novel = "And the Mountains Echoed";
-  topic = "Family"; // GS
-  let number = 123;
-  console.log("something");
-}
-otherBook();
-console.log(novel);
-console.log(topic);
-// console.log(number);
-var novel = "A Thousand Splendid Suns";
-console.log(novel);
+// function otherBook() {
+//   novel = "And the Mountains Echoed";
+//   topic = "Family"; // GS
+//   let number = 123;
+//   console.log("something");
+// }
+// otherBook();
+// console.log(novel);
+// console.log(topic);
+// // console.log(number);
+// var novel = "A Thousand Splendid Suns";
+// console.log(novel);
+
+// never stop exploring
